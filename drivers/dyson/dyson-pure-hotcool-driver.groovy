c2dcfaed64e9924a1d0e8979ec22aa5795defad6
David Pasirstein <dpasirst@yahoo.com>
2023-02-18 22:34:43 -0700

    copyright update

--- drivers/dyson/dyson-pure-hotcool-driver.groovy
+++ drivers/dyson/dyson-pure-hotcool-driver.groovy
@@ -2,7 +2,7 @@
  *  Dyson Pure Cool Link/Pure Hot Cool Hubitat Driver - UNOFFICIAL (non-"link" version)
  *  Author: David Pasirstein
  *
- *  Copyright (c) 2022 David Pasirstein
+ *  Copyright (c) 2023 David Pasirstein
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
